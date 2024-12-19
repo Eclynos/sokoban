570a12f2f73f0f6422840a0de774d3b5e41d5b05
Nathaël CARLIER <Nathael.CARLIER@etu.uca.fr>
2024-12-19 12:21:01 +0100

    indentation

--- main.c
+++ main.c
@@ -4,38 +4,40 @@
 #include "fct_utile.h"
 
 int main() {
-  if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-      printf("Erreur lors de l'initialisation de la SDL: %s\n", SDL_GetError());
-      return 1;
-  }
+    int running = 1;
 
-  SDL_Window *window = SDL_CreateWindow(
-      "Sokoban",
-      SDL_WINDOWPOS_CENTERED,
-      SDL_WINDOWPOS_CENTERED,
-      800,
-      600,
-      SDL_WINDOW_SHOWN
-  );
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        printf("Erreur lors de l'initialisation de la SDL: %s\n", SDL_GetError());
+        return 1;
+    }
 
-  if (!window) {
-      printf("Erreur lors de la création de la fenêtre: %s\n", SDL_GetError());
-      SDL_Quit();
-      return 1;
-  }
+    SDL_Window *window = SDL_CreateWindow(
+        "Sokoban",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        800,
+        600,
+        SDL_WINDOW_SHOWN
+    );
 
-  SDL_Event event;
-  int running = 1;
-  while (running) {
-      while (SDL_PollEvent(&event)) {
-          if (event.type == SDL_QUIT) {
-              running = 0;
-          }
-      }
-  }
+    if (!window) {
+        printf("Erreur lors de la création de la fenêtre: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
 
-  SDL_DestroyWindow(window);
-  SDL_Quit();
+    SDL_Event event;
 
-  return 0;
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = 0;
+            }
+        }
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
 }
